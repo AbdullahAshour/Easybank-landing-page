e5f2475e6515b786f1416c11eb51c475f78c0d0b
AbdullahAshour <abdallah2017a@hotmail.com>
2021-09-28 01:22:33 +0200

    nav overlay hero updates

--- script.js
+++ script.js
@@ -4,19 +4,30 @@ const navSlide = () =>{
     const burger = document.querySelector(".burger");
     const nav = document.querySelector(".nav-links");
     const navLinks = document.querySelectorAll(".nav-links li");
+    const overlay = document.querySelector(".overlay");
 
     burger.addEventListener("click", ()=>{
         nav.classList.toggle("nav-active");
-        navLinks.forEach((link, index) =>{
+
+        navLinks.forEach((link) =>{
             if(link.style.animation){
                 link.style.animation = ""
             }else {
-                link.style.animation = 'navLinksFade 0.4s ease 0.2s forwards';
+                link.style.animation = 'navLinksFade 0.3s ease 0.2s forwards';
             }
         });
-        burger.classList.toggle("close");
-    });
 
+        burger.classList.toggle("close");
+
+        if(overlay.style.animation && overlay.style.display){
+            overlay.style.animation = ""
+            overlay.style.display = "none"
+        }else{
+            overlay.style.animation = 'overlayAnimation 0.5s ease forwards';
+            overlay.style.display = "block"
+        }
+        
+    });
 };
 
 navSlide();
